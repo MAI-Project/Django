de3b32bb7522f30c7b28e5444e6c50e02cd10ab8
artem <bam10112002@gmail.com>
2022-07-05 18:09:18 +0300

    add magnet in models

--- web/torrentCinema/models.py
+++ web/torrentCinema/models.py
@@ -22,8 +22,10 @@ class Film(models.Model):
     name = models.CharField(max_length=255, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
     releaseDate = models.DateField(null=True, verbose_name='Дата Публикации')
-    rating = models.IntegerField(verbose_name='Рэйтинг', null=True)
-    from_torrent = models.BooleanField(verbose_name='Загрузка с торрента', null=True, default=True)
+    rating = models.IntegerField( null=True, verbose_name='Рэйтинг')
+    from_torrent = models.BooleanField(null=True, default=True, verbose_name='Загрузка с торрента')
+    imagePath = models.TextField(null=True, verbose_name='Путь до постера')
+    magnet = models.TextField(null=True, verbose_name='Магнет ссылка')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, verbose_name='Категория')
     #TODO: добавить поле магнет ссылки и пути до файла
 
