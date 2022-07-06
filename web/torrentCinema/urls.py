08b2692ae8df21152accce64de444e076f2a36ab
Vlad <kasyan.vladik125@mail.ru>
2022-07-06 13:59:58 +0300

    Added genre html commit

--- web/torrentCinema/urls.py
+++ web/torrentCinema/urls.py
@@ -3,11 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('comedy', views.comedy, name='comedy'),
-    path('drama', views.drama, name='drama'),
-    path('romance', views.romance, name='romance'),
-    path('detective', views.detective, name='detective'),
-    path('action', views.action, name='action'),
-    path('player', views.player, name='player'),
+    path('genre/<int:tag>/', views.genre, name='genre'),
     path('player/<int:filmId>/', views.player, name='player')
 ]
--- web/torrentCinema/views.py
+++ web/torrentCinema/views.py
@@ -8,34 +8,10 @@ def index(request):
     return render(request, 'main/index.html')
 
 
-def comedy(request):
-    c = Category.objects.get(name='comedy')
+def genre(request, tag):
+    c = Category.objects.get(pk=tag)
     film_list = c.film_set.all()
-    return render(request, 'main/comedy.html', {'title': 'Comedy', 'films': film_list})
-
-
-def drama(request):
-    c = Category.objects.get(name='drama')
-    film_list = c.film_set.all()
-    return render(request, 'main/drama.html', {'title': 'Drama', 'films': film_list})
-
-
-def romance(request):
-    c = Category.objects.get(name='romance')
-    film_list = c.film_set.all()
-    return render(request, 'main/romance.html', {'title': 'Romance', 'films': film_list})
-
-
-def detective(request):
-    c = Category.objects.get(name='detective')
-    film_list = c.film_set.all()
-    return render(request, 'main/detective.html', {'title': 'Detective', 'films': film_list})
-
-
-def action(request):
-    c = Category.objects.get(name='action')
-    film_list = c.film_set.all()
-    return render(request, 'main/action.html', {'title': 'Action', 'films': film_list})
+    return render(request, 'main/genre.html', {'title': c.name, 'films': film_list})
 
 
 def player(request, filmId):
